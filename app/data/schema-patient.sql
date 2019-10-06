c9f7603f01367d1183df3abb71de86fd91a68de7
jhickey1021 <54514115+jhickey1021@users.noreply.github.com>
2019-10-06 11:59:56 -0400

    create person table

--- app/data/schema-patient.sql
+++ app/data/schema-patient.sql
@@ -1,27 +1,43 @@
-USE msis_triage;
+-- USE msis_triage;
 
-CREATE TABLE Patient (
-    patientGuid VARCHAR(64) PRIMARY KEY,
+-- CREATE TABLE Patient (
+--     patientGuid VARCHAR(64) PRIMARY KEY,
+--     firstName VARCHAR(64),
+--     lastName VARCHAR(64),
+--     dob DATE DEFAULT NULL,
+--     sexAtBirth CHAR(1) DEFAULT ''
+-- );
+
+CREATE TABLE Person (
+    personID VARCHAR(64) PRIMARY KEY,
     firstName VARCHAR(64),
     lastName VARCHAR(64),
+    sexAtBirth CHAR(1) DEFAULT '',
     dob DATE DEFAULT NULL,
-    sexAtBirth CHAR(1) DEFAULT ''
-);
-
-INSERT INTO Patient (patientGuid, firstName, lastName, dob, sexAtBirth) VALUES
-("SOME-REALLY-LONG-1234", "Sylvia", "Hernandez", "2012-09-01",  "F"),
-("SOME-REALLY-SHORT-5678", "Vish", "Balasubramanian", "1950-12-15",  "M"),
-("SOME-UNIQUE-ABCDE1", "J", "Doe", "1950-00-00",  ""),
-("SOME-DUMMY-DATA", "Pepper", "Potts", "1990-01-31",  "F");
-
-CREATE TABLE PatientVisit (
-    visitId INTEGER PRIMARY KEY AUTO_INCREMENT,
-    patientGuid VARCHAR(64) UNIQUE,
-    visitDescription TEXT NOT NULL,
-    visitDateUtc DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    priority ENUM('low', 'medium', 'high') NOT NULL DEFAULT 'low'
+    phoneNumber VARCHAR(14),
+    email VARCHAR(64),
+    dateHired DATE DEFAULT NULL,
+    position VARCHAR(64),
+    isActive VARCHAR(1),
+    radioNumber INTEGER,
+    stationNumber INTEGER
 
 );
 
-INSERT INTO PatientVisit (visitId, patientGuid, visitDescription) VALUES
-(1, 'SOME-REALLY-LONG-1234', 'Anxiety from D&S');
+-- INSERT INTO Patient (patientGuid, firstName, lastName, dob, sexAtBirth) VALUES
+-- ("SOME-REALLY-LONG-1234", "Sylvia", "Hernandez", "2012-09-01",  "F"),
+-- ("SOME-REALLY-SHORT-5678", "Vish", "Balasubramanian", "1950-12-15",  "M"),
+-- ("SOME-UNIQUE-ABCDE1", "J", "Doe", "1950-00-00",  ""),
+-- ("SOME-DUMMY-DATA", "Pepper", "Potts", "1990-01-31",  "F");
+--
+-- CREATE TABLE PatientVisit (
+--     visitId INTEGER PRIMARY KEY AUTO_INCREMENT,
+--     patientGuid VARCHAR(64) UNIQUE,
+--     visitDescription TEXT NOT NULL,
+--     visitDateUtc DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+--     priority ENUM('low', 'medium', 'high') NOT NULL DEFAULT 'low'
+--
+-- );
+--
+-- INSERT INTO PatientVisit (visitId, patientGuid, visitDescription) VALUES
+-- (1, 'SOME-REALLY-LONG-1234', 'Anxiety from D&S');
