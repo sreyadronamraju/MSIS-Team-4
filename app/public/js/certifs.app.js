eb8758cbe1386abe4e51f99888146eb89c2ab338
Jordan <jofritz@iu.edu>
2019-10-19 10:50:04 -0400

    Still trying to figure out DB connection

--- app/public/js/certifs.app.js
+++ app/public/js/certifs.app.js
@@ -1,16 +1,43 @@
 var certifsApp = new Vue({
   el: '#certifsApp',
   data: {
-    certifs: []
+    members: [],
+    newmember: {}
   },
   methods: {
-    fetchCertifs() {
+    fetchMembers() {
       fetch('api/certifs/index.php')
       .then(response => response.json())
-      .then(json => { certifsApp.certifs = json })
+      .then(json => { certifsApp.certifs = json });
+    },
+
+    handleSubmit(event) {
+      fetch('api/certifs/post.php', {
+        method:'POST',
+        body: JSON.stringify(this.newcertifs),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      })
+      .then( response => response.json() )
+      .then( json => { memberApp.certifs.push(this.newcertifs) })
+      .catch( err => {
+        console.error('RECORD POST ERROR:');
+        console.error(err);
+      });
+      this.fetchCertifs();
+      this.handleReset();
+  },
+    handleReset() {
+      this.newcertifs = {
+        certfName: '',
+        certAgency: '',
+        expPeriod: '',
+      }
     }
   },
   created() {
+    this.handleReset();
     this.fetchCertifs();
   }
 });
---
+++ app/public/js/member.app.js
@@ -0,0 +1,51 @@
+var memberApp = new Vue({
+  el: '#memberApp',
+  data: {
+    members: [],
+    newmember: {}
+  },
+  methods: {
+    fetchMembers() {
+      fetch('api/members/index.php')
+      .then(response => response.json())
+      .then(json => { memberApp.members = json });
+    },
+
+    handleSubmit(event) {
+      fetch('api/members/post.php', {
+        method:'POST',
+        body: JSON.stringify(this.newmember),
+        headers: {
+          "Content-Type": "application/json; charset=utf-8"
+        }
+      })
+      .then( response => response.json() )
+      .then( json => { memberApp.members.push(this.newmember) })
+      .catch( err => {
+        console.error('RECORD POST ERROR:');
+        console.error(err);
+      });
+      this.fetchMembers();
+      this.handleReset();
+  },
+    handleReset() {
+      this.newmember = {
+        firstName: '',
+        lastName: '',
+        sexAtBirth: '',
+        dob: '',
+        phoneNumber: '',
+        email: '',
+        dateHired: '',
+        position: '',
+        isActive: '',
+        radioNumber: '',
+        stationNumber: ''
+      }
+    }
+  },
+  created() {
+    this.handleReset();
+    this.fetchMembers();    
+  }
+});
